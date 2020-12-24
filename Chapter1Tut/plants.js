f950d1ec403298337c6719faf506878763ec15b0
stephenkyle <50040013+stephenkyle@users.noreply.github.com>
2020-12-24 12:11:15 -0500

    Add files via upload

---
+++ Chapter1Tut/plants.js
@@ -0,0 +1,15 @@
+/* JavaSctipt 6th Edition
+	Chapter 1
+	Chapter case
+	
+	Tinley Xeriscapes
+	Author: Stephen Oakford
+	Date: 08/26/20
+	
+	Filename: plants.js
+*/
+
+//define variables containing img src values
+	var blanket ="images/blanket.jpg";
+	var bluestem ="images/bluestem.jpg";
+	var rugosa ="images/rugosa.jpg";
